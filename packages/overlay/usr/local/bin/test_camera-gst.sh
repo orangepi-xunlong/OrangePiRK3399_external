2aedd95a86efe5e8c3421254ffa81903348a3d17
baiywt <baiywt_gj@163.com>
2020-03-06 16:19:14 +0800

    update

--- packages/overlay/usr/local/bin/test_camera-gst.sh
+++ packages/overlay/usr/local/bin/test_camera-gst.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+BOARD=$(cat /etc/hostname)
+
+if [ ${BOARD} = "orangepi4" ];then
+	DEV="/dev/video0"
+elif [ ${BOARD} = "orangepirk3399" ]; then
+	DEV="/dev/video5"
+fi
+
 export DISPLAY=:0.0
-su orangepi -c "gst-launch-1.0 rkisp device=/dev/video1 io-mode=1 analyzer=1 enable-3a=1 path-iqf=/etc/cam_iq.xml \
+su orangepi -c "gst-launch-1.0 rkisp device=${DEV} io-mode=1 analyzer=1 enable-3a=1 path-iqf=/etc/cam_iq.xml \
 	! video/x-raw,format=NV12,width=1280,height=720, framerate=30/1 ! videoconvert ! autovideosink"
-#su orangepi -c "gst-launch-1.0 rkisp device=/dev/video1 io-mode=1 analyzer=1 enable-3a=1 path-iqf=/etc/cam_iq.xml \
-#	! video/x-raw,format=NV12,width=1280,height=720, framerate=30/1 ! videoconvert ! ximagesink"
--- packages/overlay/usr/local/bin/test_camera-record.sh
+++ packages/overlay/usr/local/bin/test_camera-record.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
   
+BOARD=$(cat /etc/hostname)
+
+if [ ${BOARD} = "orangepi4" ];then
+	DEV="/dev/video0"
+elif [ ${BOARD} = "orangepirk3399" ]; then
+	DEV="/dev/video5"
+fi
+
 export DISPLAY=:0.0
 
 su orangepi -c " \
-		gst-launch-1.0 rkisp num-buffers=512 device=/dev/video1 io-mode=1 ! \
+		gst-launch-1.0 rkisp num-buffers=512 device=${DEV} io-mode=1 ! \
 		video/x-raw,format=NV12,width=1280,height=720,framerate=30/1 ! \
 		mpph264enc ! queue ! h264parse ! mpegtsmux ! \
 		filesink location=/home/orangepi/output.ts \
