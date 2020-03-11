4483d9c05874af52ced044b6887bc272289aa79f
baiywt <baiywt_gj@163.com>
2020-03-11 09:54:32 +0800

    remove bluetooth.sh

--- packages/overlay/usr/local/bin/bluetooth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rfkill unblock all
- echo "0" > /sys/class/rfkill/rfkill0/state
- echo "1" > /sys/class/rfkill/rfkill0/state
- echo "" > /dev/ttyS0
-
-brcm_patchram_plus --patchram /system/etc/firmware/BCM4345C5.hcd \
-	--baudrate 115200 \
---enable_lpm --enable_hci --no2bytes /dev/ttyS0 &
